9bf763360799ebf02fded21bec9f396f2685d46a
NeeteshKing21 <neeteshking21@gmail.com>
2021-09-27 21:59:16 +0530

    recreate index.js

--- server/database.js
+++ server/database.js
@@ -1,22 +1,18 @@
-import "firebase/compact/firestore"
-const { initializeApp } = require('firebase/app');
-const { getFirestore, collection, getDocs } = require('firebase/firestore');
-
+import { initializeApp } from 'firebase/app';
+import {getFirestore, getDoc, updateDoc, doc}  from '@firebase/firestore/lite';
+// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD8rhs2wGA9XJgxFepxwCiLI9c7kVXxOC0",
-    authDomain: "memory-save-e458c.firebaseapp.com",
-    databaseURL: "https://memory-save-e458c-default-rtdb.firebaseio.com",
-    projectId: "memory-save-e458c",
-    storageBucket: "memory-save-e458c.appspot.com",
-    messagingSenderId: "487525455540",
-    appId: "1:487525455540:web:de3eb1fc78de4d8bbd29e1",
-    measurementId: "G-BYM3J1PQJX"
-  }
+  apiKey: "AIzaSyD8rhs2wGA9XJgxFepxwCiLI9c7kVXxOC0",
+  authDomain: "memory-save-e458c.firebaseapp.com",
+  databaseURL: "https://memory-save-e458c-default-rtdb.firebaseio.com",
+  projectId: "memory-save-e458c",
+  storageBucket: "memory-save-e458c.appspot.com",
+  messagingSenderId: "487525455540",
+  appId: "1:487525455540:web:de3eb1fc78de4d8bbd29e1",
+  measurementId: "G-BYM3J1PQJX"
+};
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-const db = getFirestore(app)
-
-const User = collection(db, 'Users')
-
-module.exports = {User, db}
+export {db}
--- server/index.js
+++ server/index.js
@@ -1,28 +1,22 @@
-const express = require('express')
-const { getFirestore, collection, getDocs, setDocs } = require('firebase/compact/firestore');
-const {User} = require('./database')
-const cors = require('cors')
-const {db} = require('./database')
-const PORT = process.env.port || 5000
-const app = express()
-app.use(cors())
+import express  from "express"
+import cors from "cors"
+import morgan from "morgan"
+
+const app = express();
+app.use(cors());
 app.use(express.json())
-app.get('/', (req,res)=>res.send("Home page"))
-app.post('/add', async (req, res)=>{
-    console.log(req.body)
-    let doc = db.collection('Users')
-    try{
-        await doc.set({
-            email: req.body.User.email,
-            password: req.body.User.password
-        })
-        res.send({message:"Successfully data added"})
-    }
-    catch(e){
-        console.log(e)
-        res.send({message: "Error"});
-    }
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.send({"Hello":"Yes"});
+});
+
+app.post('/add', (req, res)=>{
+    const data = req.body;
+    console.log(data)
+    res.send('wow');
 })
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+
+app.listen(5000, () => {
+    console.log(`Server started on http://localhost:5000`);
 });

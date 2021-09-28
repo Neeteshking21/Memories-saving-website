66f479a9137d1357c3e3536eb4cfe10f115b0bd1
NeeteshKing21 <neeteshking21@gmail.com>
2021-09-28 15:53:58 +0530

    Firebase connection successfully established:🚀

--- server/database.js
+++ server/database.js
@@ -1,18 +1,27 @@
 import { initializeApp } from 'firebase/app';
-import {getFirestore, getDoc, updateDoc, doc}  from '@firebase/firestore/lite';
+import {getFirestore, getDoc, updateDoc, addDoc, collection}  from '@firebase/firestore/lite';
+import dotenv from 'dotenv'
+dotenv.config()
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD8rhs2wGA9XJgxFepxwCiLI9c7kVXxOC0",
-  authDomain: "memory-save-e458c.firebaseapp.com",
-  databaseURL: "https://memory-save-e458c-default-rtdb.firebaseio.com",
-  projectId: "memory-save-e458c",
-  storageBucket: "memory-save-e458c.appspot.com",
-  messagingSenderId: "487525455540",
-  appId: "1:487525455540:web:de3eb1fc78de4d8bbd29e1",
-  measurementId: "G-BYM3J1PQJX"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+// const docRed = await addDoc(collection(db, "Cities"), {
+//   name: "Neetesh",
+//   city: "India"
+// })
+
+// console.log("Data base connected")
 
 export {db}
--- server/index.js
+++ server/index.js
@@ -1,22 +1,34 @@
 import express  from "express"
 import cors from "cors"
 import morgan from "morgan"
-
+import {getDoc, updateDoc, addDoc, collection}  from 'firebase/firestore/lite';
+import { db } from "./database.js"
+const PORT = process.env.PORT || 5000
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json({limit: "50mb", extended: true}))
+app.use(express.urlencoded({limit: "50mb", extended: true}))
 app.use(morgan('dev'))
+// app.use(db)
+const docRed =  await addDoc(collection(db, "Cities"), {
+    title: 'Firts',
+    description:'this is my first meemory save',
+    data: Date.now()
+
+}).then(()=> app.listen(PORT, () => {console.log(`Server started on http://localhost${PORT}`)}))
+  .catch((err)=>console.log('Data Base connection error \n ' + err))
+    
+
 
 app.get('/', (req, res) => {
     res.send({"Hello":"Yes"});
 });
 
 app.post('/add', (req, res)=>{
+
     const data = req.body;
-    console.log(data)
-    res.send('wow');
+
+    console.log(data.name)
+
 })
 
-app.listen(5000, () => {
-    console.log(`Server started on http://localhost:5000`);
-});

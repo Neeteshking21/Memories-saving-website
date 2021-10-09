b665f1ce8a506ba455670191e3578feacc9e55d3
NeeteshKing21 <neeteshking21@gmail.com>
2021-10-09 18:47:19 +0530

    Add @mui/styles and Dockerfile and .dockerignore in server side

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import React from 'react'
 import Form from './components/Forms/Form'
 import Posts from './components/Posts/Posts'
 import {Container, AppBar, Typography, Grow, Grid} from '@mui/material'
+import Memories from './images/images.png'
+import useStyles from './Style'
 
 const App = () => {
+
+    const classes = useStyles();
+
     return (
-       <Container maxWidth="lg">              
-         <AppBar position="static" color="inherit">
+       <Container maxWidth="lg"  >              
+         <AppBar className="appbar" position="static" color="inherit" bgcolor="black">
            <Typography varaint="h2" align="center">Memories</Typography>
-           <img src="./" alt="memories" height="60" />
+           <img src={Memories} alt="memories" height="300" width="300" />
          </AppBar>
          <Grow in>
            <Container>

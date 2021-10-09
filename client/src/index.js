05101c31d59786c1c43d8640817c2137600f0291
NeeteshKing21 <neeteshking21@gmail.com>
2021-10-09 23:49:40 +0530

    Add redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, componse } from 'redux'
+import thunk from 'redux-thunk'
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
